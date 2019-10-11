938177e88aed68af85338a702093b6e6fcb18ac0
siccoo <obichilaka@gmail.com>
2019-10-11 15:33:52 +0100

    installed react-icons in the package.json, calling of some properties of the react-icons in Sidebar.js

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar} from 'react-icons/fa';
 
 class Sidebar extends Component {
-    
-
     render() {
         return (
             <div className="sidebar" data-testid="sidebar">
-                <ul className="sidebar-generic"> 
-                    <li>Inbox</li>
-                    <li>Today</li>
-                    <li>Next 7 days</li>
+                <ul className="sidebar__generic"> 
+                    <li>
+                        <span><FaInbox /></span>
+                        <span>Inbox</span>
+                    </li>
+                    <li>
+                        <span><FaRegCalendar /></span>
+                        <span>Today</span>
+                    </li>
+                    <li>
+                        <span><FaRegCalendarAlt /></span>
+                        <span>Next 7 days</span>
+                    </li>
                 </ul>
             </div>
         )
     }
 }
+
+// b = block;
+// e = element;
+// m = modifier
 
 export default Sidebar
 
