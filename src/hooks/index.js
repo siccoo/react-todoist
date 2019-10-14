b1219205f0421ce13c0fbc90ba22f075c4870e0f
siccoo <obichilaka@gmail.com>
2019-10-14 18:01:48 +0100

    Modification of index.js file in hooks folder

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,7 +1,7 @@
-import {useState, useEffect} from 'react';
-import {firebase} from '../firebase';
+import { useState, useEffect } from 'react';
+import { firebase } from '../firebase';
 
-const collatedTasks = () => {}
+const collatedTasks = () => { }
 
 export const useTasks = selectedProject => {
     const [tasks, setTasks] = useState([
@@ -12,15 +12,34 @@ export const useTasks = selectedProject => {
 
     useEffect(() => {
         let unsubscribe = firebase
-        .firestore().collection().where('userId', '==', 'jFyKWUiivyasdf123Ytv')
+            .firestore().collection().where('userId', '==', 'jFyKWUiivyasdf123Ytv')
 
         unsubscribe = selectedProject && !collatedTasksExist(selectedProject) ?
-        (unsubscribe = unsubscribe.where('projectId', '==', selectedProject)) 
-        : selectedProject === 'TODAY'
-        ? (unsubscribe = unsubscribe.where(
-            'date', 
-            '==', 
-            moment().format('DD/MM/YYYY')
-            )) : selectedProject === 'INBOX' || selectedProject === 0
-    }, [])
-}
+            (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
+            : selectedProject === 'TODAY'
+                ? (unsubscribe = unsubscribe.where(
+                    'date',
+                    '==',
+                    moment().format('DD/MM/YYYY')
+                )) : selectedProject === 'INBOX' || selectedProject === 0
+                    ? (unsubscribe = unsubscribe.where('date', '==', ''))
+                    : unsubscribe;
+
+        unsubscribe = unsubscribe.onSnapshot(snapshot => {
+            const newTasks = snapshot.docs.map(task => ({
+                id: task.id,
+                ...task.data(),
+            }));
+
+            setTasks(
+                selectedProject === 'NEXT_7'
+                    ? newTasks.filter(
+                        task => moment(task.date, 'DD-MM-YYYY')
+                            .diff(moment(), 'days') <= 7 &&
+                            task.archived !== true
+                    )
+                    : newTasks.filter(task => task.archived !== true)
+            )
+        })
+    }, [selectedProject]);
+};
