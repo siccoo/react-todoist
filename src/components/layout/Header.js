41b3803383868900cd770642336da83c4ac64a11
siccoo <obichilaka@gmail.com>
2019-11-01 12:09:20 +0100

    styling of the todoist logo on the nav bar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,7 +7,7 @@ export const Header = () => {
         <header className="header" data-testid="header">
             <nav>
                 <div className="logo">
-                    <img src="/images/logo.jpg" alt="Todoist" />
+                    <img src="/images/Todoist.jpg" alt="Todoist" />
                 </div>
                 <div className="settings">
                     <ul>
